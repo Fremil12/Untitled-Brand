e6a544c5a2f71d7bfd68380af1dad900a1acb40b
KalauzBalazs <77264674+KalauzBalazs@users.noreply.github.com>
2024-05-31 19:49:59 +0200

    kép és szöveg kezelése együtt

--- test.js
+++ test.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
-import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, onSnapshot, updateDoc,setDoc} from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js"
+//képekhez
+import { getStorage, ref as sRef, uploadBytesResumable, getDownloadURL } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-storage.js";
+//szöveghez
+import { getFirestore, collection, getDocs, addDoc, deleteDoc, doc, onSnapshot, updateDoc, setDoc, getDoc } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-firestore.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyAq7OU5i2WgamVcWwXtvLTjP20L5449HJo",
@@ -14,20 +17,125 @@ const firebaseConfig = {
 initializeApp(firebaseConfig);
 
 const db = getFirestore();
+const storage = getStorage();
 let colRef = collection(db, "datas");
 
-let name, data, data2, id;
+let name, description, price, Imgname, id;
+
+//kép ---------------------------------------
+
+
+
+
+var files = [];
+var reader = new FileReader();
+
+let namebox = document.querySelector(".namebox");
+let extlab = document.querySelector(".extlab");
+let myimg = document.querySelector(".uploadedImg");
+let proglab = document.querySelector(".upProgress");
+let SelBtn = document.querySelector(".selbtn");
+let UpBtn = document.querySelector(".upbtn");
+let DownBtn = document.querySelector(".downbtn");
+
+var input = document.createElement("input");
+input.type = "file";
+
+input.onchange = e => {
+    files = e.target.files;
+
+    var extension = GetFileExtension(files[0]);
+    var name = GetFileName(files[0]);
+    namebox.value = name;
+    extlab.innerHTML = extension;
+    reader.readAsDataURL(files[0]);
+};
+
+reader.onload = function () {
+    myimg.src = reader.result;
+};
+
+SelBtn.addEventListener("click", function () {
+    input.click();
+});
+
+function GetFileExtension(file) {
+    var temp = file.name.split(".");
+    var ext = temp.slice((temp.length - 1), (temp.length));
+    return "." + ext[0];
+}
+
+function GetFileName(file) {
+    var temp = file.name.split(".");
+    var fname = temp.slice(0, -1).join(".");
+    return fname;
+}
+
+async function UploadProcess() {
+    var ImgToUpload = files[0];
+    var ImgName = namebox.value + extlab.innerHTML;
+    const metaData = {
+        contentType: ImgToUpload.type
+    };
+
+    const storageRef = sRef(storage, "Images/" + ImgName);
+    const UploadTask = uploadBytesResumable(storageRef, ImgToUpload, metaData);
+
+    UploadTask.on("state_changed", (snapshot) => {
+        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+        proglab.innerHTML = "Upload " + progress + "%";
+    },
+        (error) => {
+            console.log("error: image not uploaded!", error);
+        },
+        () => {
+            getDownloadURL(UploadTask.snapshot.ref).then((downloadURL) => {
+                console.log(downloadURL);
+                setDoc(doc(db, "ImageLinks", namebox.value), {
+                    ImageURL: downloadURL
+                });
+            });
+        }
+    );
+}
+
+UpBtn.addEventListener("click", function () {
+    UploadProcess();
+});
+
+async function GetImagefromFirestore() {
+    var name = namebox.value;
+    var ref = doc(db, "ImageLinks", name);
+    const docSnap = await getDoc(ref);
+    if (docSnap.exists()) {
+        myimg.src = docSnap.data().ImageURL;
+    } else {
+        console.log("No such document!");
+    }
+}
+
+DownBtn.addEventListener("click", function () {
+    GetImagefromFirestore();
+});
+
+
+
+
+
+//szöveges adatok --------------------------
 
 // Writing data
 function addData() {
   name = document.querySelector(".name").value;
-  data = document.querySelector(".data").value;
-  data2 = document.querySelector(".data2").value;
+  description = document.querySelector(".description").value;
+  price = document.querySelector(".price").value;
+  id = document.querySelector(".id").value; 
+
 
   addDoc(colRef, {
     name: name,
-    data: data,
-    data2: data2,
+    description: description,
+    price: price,
   })
     .then((docRef) => {
       console.log("Document written with ID", docRef.id);
@@ -39,15 +147,15 @@ function addData() {
 
 function addDataWithId() {
   name = document.querySelector(".name").value;
-  data = document.querySelector(".data").value;
-  data2 = document.querySelector(".data2").value;
+  description = document.querySelector(".description").value;
+  price = document.querySelector(".price").value;
   id = document.querySelector(".id").value; 
 
   const docRef = doc(db, "datas", id);
   setDoc(docRef, {
     name: name,
-    data: data,
-    data2: data2,
+    description: description,
+    price: price,
   })
   .then(() => {
     console.log("Document written with ID", id);
@@ -59,9 +167,13 @@ function addDataWithId() {
 
 // Get single document data
 function getData() {
+
+  id = document.querySelector(".id").value; 
   const docRef = doc(db, "datas", id);
   onSnapshot(docRef, (doc) => {
     if (doc.exists()) {
+      let singleElement=document.querySelector(".one");
+      singleElement.innerHTML=doc.data().name + " " + doc.data().description + " " + doc.data().price;
       console.log(doc.data(), doc.id);
     } else {
       console.log("No such document!");
@@ -74,14 +186,15 @@ function getData() {
 // Update data
 function updateData() {
   name = document.querySelector(".name").value;
-  data = document.querySelector(".data").value;
-  data2 = document.querySelector(".data2").value;
+  description = document.querySelector(".description").value;
+  price = document.querySelector(".price").value;
+  id = document.querySelector(".id").value; 
 
   const docRef = doc(db, "datas", id);
   updateDoc(docRef, {
     name: name,
-    data: data,
-    data2: data2,
+    description: description,
+    price: price,
   })
     .then(() => {
       console.log("Document successfully updated!");
@@ -93,6 +206,7 @@ function updateData() {
 
 // Delete data
 function deleteData() {
+  id = document.querySelector(".id").value; 
   const docRef = doc(db, "datas", id);
   deleteDoc(docRef)
     .then(() => {
@@ -115,7 +229,7 @@ function getAll() {
       let allElement = document.querySelector(".all");
       allElement.innerHTML = ""; // Clear previous data
       for (let i = 0; i < datas.length; i++) {
-        allElement.innerHTML += datas[i].name + " " + datas[i].data + " " + datas[i].data2 + "<br>";
+        allElement.innerHTML += datas[i].name + " " + datas[i].description + " " + datas[i].price + "<br>";
       }
     })
     .catch((error) => {
@@ -130,9 +244,9 @@ onSnapshot(colRef, (querySnapshot) => {
     datas.push({ ...doc.data(), id: doc.id });
   });
   console.log(datas);
-  document.querySelector(".all").innerHTML = "<br><br>"; // Clear previous data
+  document.querySelector(".all").innerHTML = ""; // Clear previous data
   for (let i = 0; i < datas.length; i++) {
-    document.querySelector(".all").innerHTML += datas[i].name + " " + datas[i].data + " " + datas[i].data2 +" - "+datas[i].id+"<br>";
+    document.querySelector(".all").innerHTML += datas[i].name + " " + datas[i].description + " " + datas[i].price +" - "+datas[i].id+"<br>";
   }
 }, (error) => {
   console.error("Error getting documents:", error);
@@ -149,17 +263,18 @@ document.querySelector(".adddatawithid").addEventListener("click", function () {
 
 
 document.querySelector(".get").addEventListener("click", function () {
-  id = document.querySelector(".id").value;
   getData();
 });
 document.querySelector(".update").addEventListener("click", function () {
-  id = document.querySelector(".id").value;
   updateData();
 });
 document.querySelector(".delete").addEventListener("click", function () {
-  id = document.querySelector(".id").value;
   deleteData();
 });
 document.querySelector(".getall").addEventListener("click", function () {
   getAll();
 });
+
+
+
+
